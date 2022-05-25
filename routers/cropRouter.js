ea03bbcad9509f55e8a8ff49b9fbcbefe90868e8
srinivas-reddy-a <it@arraykart.com>
2022-05-25 12:20:40 +0530

    cropwise filters corrections

--- routers/cropRouter.js
+++ routers/cropRouter.js
@@ -30,12 +30,14 @@ cropRouter.get(
     })
 )
 
-//get cropwise
+//get cropwise categories
 cropRouter.get(
     '/:crop/product/',
     expressAsyncHandler(async (req, res) => {
         const limit = req.query.limit || 1000000;
         const crop = req.params.crop;
+        const orderby = req.query.orderby || 'category';
+        const order = req.query.order || 'asc';
         try {
             await db('product')
             .where((qb)=>{                
@@ -48,8 +50,8 @@ cropRouter.get(
             })
             .select('id', 'category')
             .limit(limit)
-            .orderBy('category', 'asc')
-            .then(products => {
+            .orderBy(orderby, order)
+            .then(categories => {
                 // products.forEach(p => {
                 //     console.log(p)
                 //     translateJSON({
@@ -58,7 +60,7 @@ cropRouter.get(
                 // })             
                 res.status(200).send({
                     success:true,
-                    products:products
+                    categories:categories
                 })
             })
             .catch(err => res.status(400).send({
@@ -116,81 +118,5 @@ cropRouter.get(
             })
         }
     }))
-
-
-cropRouter.get(
-    '/product/',
-    expressAsyncHandler(async (req, res) => {
-        const limit = req.query.limit || 1000000;
-        const orderby = req.query.orderby || 'name';
-        const order = req.query.order || 'desc';
-        try {
-            await db('product')
-            .where((qb)=>{
-                if(req.query.name){
-                    qb.where('name', 'like', `%${req.query.name}%`)
-                }
-                if(req.query.technical_name){
-                    qb.where('technical_name', 'like', `%${req.query.technical_name}%`)
-                }
-                if(req.query.category){
-                    qb.where('category', 'like', `%${req.query.category}%`)
-                }                
-                if(req.query.crop){
-                    qb.where('target_field_crops', 'like', `%${req.query.crop}%`)
-                    .orWhere('target_vegetable_crops', 'like', `%${req.query.crop}%`)
-                    .orWhere('target_fruit_crops', 'like', `%${req.query.crop}%`)
-                    .orWhere('target_plantation_crops', 'like', `%${req.query.crop}%`)
-                }
-                if(req.query.brand){
-                    qb.where('brand', 'like', `%${req.query.brand}%`)
-                }
-                if(req.query.price){
-                    qb.where('price', '<=', req.query.price)
-                }
-                if(req.query.disease){
-                    qb.where('target_disease', 'like', `%${req.query.disease}%`)
-                }
-                if(req.query.search){
-                    qb.where('target_field_crops', 'like', `%${req.query.search}%`)
-                    .orWhere('target_vegetable_crops', 'like', `%${req.query.search}%`)
-                    .orWhere('target_fruit_crops', 'like', `%${req.query.search}%`)
-                    .orWhere('target_plantation_crops', 'like', `%${req.query.search}%`)
-                    .orWhere('name', 'like', `%${req.query.search}%`)
-                    .orWhere('brand', 'like', `%${req.query.search}%`)
-                    .orWhere('target_disease', 'like', `%${req.query.search}%`)
-                    .orWhere('category', 'like', `%${req.query.search}%`)
-                    .orWhere('technical_name', 'like', `%${req.query.technical_name}%`)
-                }
-            })
-            .limit(limit)
-            .orderBy(orderby, order)
-            .then(products => {
-                // products.forEach(p => {
-                //     console.log(p)
-                //     translateJSON({
-                //         a:"road"
-                //     }).then(console.log);
-                // })             
-                res.status(200).send({
-                    success:true,
-                    products:products
-                })
-            })
-            .catch(err => res.status(400).send({
-                success:false,
-                message:"db error"
-            }))
-        } catch (error) {
-            res.status(500).send({
-                success:false,
-                message:"server error"
-            })
-        }
-    }))
-
-
-
-
 
 export default cropRouter;
